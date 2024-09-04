6eae1533ae61fabf02da3e4c36cb47afcde73cad
Niza-Platform-FE <niza-fe@gate.me>
2024-09-04 22:38:41 +0800

    Site updated: 2024-09-04 22:38:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/04/hello-world/","2024/09/04/我的第一篇博客-Hexo-Hello/"];function toRandomPost(){
+var posts=["2024/09/04/hello-world/","2024/09/05/hexo-分享/","2024/09/04/我的第一篇博客-Hexo-Hello/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
